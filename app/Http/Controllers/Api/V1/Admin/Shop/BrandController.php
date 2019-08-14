359eb49e85988a19048b19bb4d1c43dd2e19e1ad
letrananhvu <vule5726@gmail.com>
2019-08-14 18:12:07 +0700

    todo delete brand

--- app/Http/Controllers/Api/V1/Admin/Shop/BrandController.php
+++ app/Http/Controllers/Api/V1/Admin/Shop/BrandController.php
@@ -16,12 +16,12 @@ class BrandController extends Controller
      */
     public function index()
     {
-        try{
+        try {
             $brands = Brand::withgetOrderBy(Input::get('order_by'))
                 ->searchKeyword(Input::get('keyword'))
                 ->paginate(5);
-        }catch(\Exception $e) {
-            return response(['message'=> $e->getMessage()], 400);
+        } catch (\Exception $e) {
+            return response(['message' => $e->getMessage()], 400);
         }
         return response([
             'data' => $brands
@@ -31,7 +31,7 @@ class BrandController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -42,20 +42,25 @@ class BrandController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        try {
+            $brand = Brand::findOrFail($id);
+            return response(['data' => $brand], 200);
+        } catch (\Exception $e) {
+            return response(['message' => $e->getMessage()], 404);
+        }
     }
 
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -66,11 +71,22 @@ class BrandController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        try {
+            $brand = Brand::find($id);
+            $products = $brand->products();
+            foreach ($products as $product) {
+                $product->brand_id = null;
+                $product->save();
+            }
+            $brand->delete();
+            return response(['message' => 'delete successfully'], 200);
+        }catch(\Exception $e){
+            return response(['message' => $e->getMessage()], 404);
+        }
     }
 }
